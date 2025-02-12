20fb81ffe2370d2bd8291010450df2e669101870
FurqanRandera <muhammadfurqanrandera@gmail.com>
2025-02-12 13:26:17 +0000

    Third Commit

--- login/signup section furqanrandera/signup-info.php
+++ login/signup section furqanrandera/signup-info.php
@@ -1,20 +1,7 @@
 <?php
 
-// Here is how I retrieved the data from the HTML signup form
-$Username = $_POST['Username'];
-$dropdown = $_POST['dropdown'];
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
+$mysqli = new mysqli("localhost", "2351031", "63m88v", "db2351031");
 
-// Here is where the database analyst will put his details to connect to his phpmyadmin database.
-$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-
-
-// Here is just a message incase the connection to the database fails
-if (mysqli_connect_error()) {
-    echo "Could not connect to MySQL database: " . $mysqli -> connect_error;
-} else {
-    
-}
+$sql = "SELECT * FROM ORDER BY weather_when DESC limit 1";
 
 ?>
