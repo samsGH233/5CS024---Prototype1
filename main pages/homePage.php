5320f510e430521f6d90e6bafc0c8f8f1b9dd84e
samsGH233 <S.fatogun@wlv.ac.uk>
2025-03-14 14:54:57 +0000

    PHP home page code committed

---
+++ main pages/homePage.php
@@ -0,0 +1,68 @@
+<?php include "navBar.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FreshStart | homePage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+        <div class="container">
+            <div class="d-sm-flex align-items-center justify-content-between">
+                <div>
+                    <h1 class="header text-dark" style="margin-top: 20px;">Welcome to <span class="section text-primary">FreshStart</span></h1>
+                    <p class="lead my-4">As you use this app druing your experience at the University of Wolverhampton Open Day, this platfrom should give you a fresh perspective about life at university and should anticipate you for your new journey !</p>
+                </div>
+                <img class="img-fluid w-50 img-thumbnail d-none d-sm-block" src="wolv-uni.webp" style="width: 180px; height: 180px; margin-top: 20px;">
+            </div>
+        </div>
+
+        <br><br><br>
+
+    <!-- Open Day section -->
+     <section class="bg-primary text-light p-5" style="height:500px; margin-top: 90px;">
+        <div class="container">
+                <h3 class="mb-3 mb-md-0" style="font-size: 50px;" >Why go to a University Open Day ?
+                </h3>
+                <div class="od-container" style="display: flex; align-items: center; margin-top: 10px;  gap: 500px; font-size: 17px;">
+                <ul class="bullet-points">
+                    <li>More familiratiy with the facilites and the staff</li>
+                    <li>More familiratiy with the facilites and the staff</li>
+                    <li>More familiratiy with the facilites and the staff</li>
+                    <li>More familiratiy with the facilites and the staff</li>
+                    <li>More familiratiy with the facilites and the staff</li>
+                </ul>
+                
+                    <iframe width="420" height="315" src="https://www.youtube.com/embed/QDNxCMspP0g"></iframe>
+                </div>
+           
+               
+            </div>
+        </div>
+        </div>
+     </section>
+
+
+
+
+    <!-- Boxes -->
+    <section class="p-5">
+        <header class = "text-center">Contents</header>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md"><a href="#" class="btn" style="background-color:yellow; width: 200px; height: 50px;">ALUMNI</a></div>
+                <br>
+                <div class="col-md"><a href="#" class="btn" style="background-color:blue; width: 200px; height: 50px;" >TIPS</a></div>
+                <br>
+                <div class="col-md"><a href="#" class="btn" style="background-color: green; width: 200px; height: 50px;">FEEDBACK</a></div>
+            </div>
+        </div>
+    </section>
+
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</body>
+</html>
